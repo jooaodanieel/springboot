5060d6684e3c4062caf99fe57ef6ef076363cc2a
João Daniel <jotaf.daniel@gmail.com>
2021-09-13 21:45:09 -0300

    add student controller
    extract code from main application to the controller

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1L, "João", 29, LocalDate.of(1991, Month.OCTOBER, 4), "jotaf.daniel@gmail.com")
+        );
+    }
+}
